b13e2fe2f20e7ed5b712c2a6b7eab4d396180675
Emile Belot <emile.belot@gmail.com>
2024-01-16 22:41:22 +0100

    update code VBT

--- Trading 2.0/VBT Training.py
+++ Trading 2.0/VBT Training.py
@@ -6,20 +6,19 @@ end_date = datetime.datetime.now()
 start_date = end_date - datetime.timedelta(days=10)
 
 btc_price = vbt.YFData.download(
-    'BTC-USD',
+    ['BTC-USD','ETH-USD'],
+    missing_index='drop',
     interval='60m',
     start=start_date,
-    end=end_date,
-    missing_index='drop').get('Close')
+    end=end_date).get('Close')
 
 rsi = vbt.RSI.run(btc_price, window=14)
 
-
-def custom_indicator(close, rsi_window=14, ma_window = 50):
+def custom_indicator(close, rsi_window, ma_window):
     rsi = vbt.RSI.run(close, window=rsi_window).rsi.to_numpy()
     ma = vbt.MA.run(close, ma_window).ma.to_numpy()
-    trend = np.where(rsi>70, -1, 0)
-    trend = np.where(rsi<30, 1, 0)
+    trend = np.where(rsi > 70, -1, 0)
+    trend = np.where( (rsi < 30) & (close<ma), 1, trend)
     return trend
 
 
@@ -32,18 +31,23 @@ indic = vbt.IndicatorFactory(
 ).from_apply_func(custom_indicator, rsi_window=21, ma_window=50)
 
 res = indic.run(btc_price,
-                rsi_window=21,
-                ma_window = 50)
+                rsi_window = [10,20,30],
+                ma_window = [20,50,200],
+                param_product=True)
 
 
-print(res.value)
+# print(res.value)
+
+entries = res.value == 1.0
+exits = res.value == -1.0
 
 #
 # entries = rsi.rsi_crossed_below(30)
 # exits = rsi.rsi_crossed_above(70)
 
-# pf = vbt.Portfolio.from_signals(btc_price,entries,exits)
+pf = vbt.Portfolio.from_signals(btc_price,entries,exits)
 
+print(pf.total_return()>0)
 
 # print(pf.stats())
 #
